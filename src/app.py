68c70a019cdb893e86a599ee3ffd5d99c9edfc89
chris <christianmatabaro@gmail.com>
2020-04-07 07:04:07 +0200

    rendering html pages through our different routes

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # instantiate the object of the class flask
 app = Flask(__name__)
@@ -6,11 +6,13 @@ app = Flask(__name__)
 # creating our first route as home page
 @app.route('/')
 def home():
-    return 'Homepage content'
+    # render_tempalte help to access different html pages
+    return render_template('home.html')
 
 @app.route('/about/')
 def about():
-    return 'About content goes here'
+    # rendering the about page
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
